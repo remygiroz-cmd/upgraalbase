370443fd88dd81dfdd1ae561f047f8d9e5e17707
Frenchy Sushi <remy.giroz@gmail.com>
2026-01-24 15:44:53 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -345,7 +345,7 @@ export default function Layout({ children, currentPageName }) {
         "min-h-screen transition-all duration-300",
         "pt-16 lg:pt-0 lg:pl-72"
       )}>
-        <div className="p-4 lg:p-6">
+        <div className="p-4 lg:p-6 pb-24 lg:pb-6">
           {children}
         </div>
       </main>
