5c0481919d9b05f99b4273ad2edf325aeff747e8
Frenchy Sushi <remy.giroz@gmail.com>
2026-01-26 07:11:21 +0000

    File changes

--- functions/sendOrderEmail.ts
+++ functions/sendOrderEmail.ts
@@ -1,6 +1,7 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 import { jsPDF } from 'npm:jspdf@2.5.2';
 
+// Version avec masquage des prix
 Deno.serve(async (req) => {
   try {
     const base44 = createClientFromRequest(req);
