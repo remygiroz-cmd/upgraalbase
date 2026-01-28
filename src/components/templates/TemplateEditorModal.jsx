675ee6397ba16869b9745e8e1eb09176a8195b4e
Frenchy Sushi <remy.giroz@gmail.com>
2026-01-28 22:53:03 +0000

    File changes

--- src/components/templates/TemplateEditorModal.jsx
+++ src/components/templates/TemplateEditorModal.jsx
@@ -200,7 +200,7 @@ export default function TemplateEditorModal({ open, onOpenChange, template, onSu
               <AlertCircle className="w-5 h-5 text-blue-600 flex-shrink-0 mt-0.5" />
               <div className="text-sm text-blue-800">
                 <p className="font-semibold">Variables disponibles:</p>
-                <p>{{prenom}}, {{nom}}, {{naissance}}, {{adresse}}, {{poste}}, {{debut}}, {{fin}}, {{heures}}, {{taux}}, {{salaireBrut}}, {{motifCDD}}, etc.</p>
+                <p className="font-mono text-xs">{`{{prenom}}, {{nom}}, {{naissance}}, {{adresse}}, {{poste}}, {{debut}}, {{fin}}, {{heures}}, {{taux}}, {{salaireBrut}}, {{motifCDD}}, etc.`}</p>
               </div>
             </div>
 
